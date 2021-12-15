f3dd4b99152346d24208e99614467cd9fe903b9c
Marusya-Belova <m9175797050@gmail.com>
2021-12-15 12:23:58 +0300

    Reformat Code

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -12,43 +12,47 @@ class SQRServiceTest {
         int minLimit = 100;
         int maxLimit = 9801;
         int expected = 90;
-        int actual = sqrService.findSqr(minLimit,maxLimit);
-        assertEquals(expected,actual);
+        int actual = sqrService.findSqr(minLimit, maxLimit);
+        assertEquals(expected, actual);
     }
+
     @Test
     void shouldFindSqr0() {
         SQRService sqrService = new SQRService();
         int minLimit = 0;
         int maxLimit = 0;
         int expected = 0;
-        int actual = sqrService.findSqr(minLimit,maxLimit);
-        assertEquals(expected,actual);
+        int actual = sqrService.findSqr(minLimit, maxLimit);
+        assertEquals(expected, actual);
     }
+
     @Test
     void shouldFindSqrBeforeLimit() {
         SQRService sqrService = new SQRService();
         int minLimit = 0;
         int maxLimit = 99;
         int expected = 0;
-        int actual = sqrService.findSqr(minLimit,maxLimit);
-        assertEquals(expected,actual);
+        int actual = sqrService.findSqr(minLimit, maxLimit);
+        assertEquals(expected, actual);
     }
+
     @Test
     void shouldFindSqrAfterLimit() {
         SQRService sqrService = new SQRService();
         int minLimit = 9802;
         int maxLimit = 5000000;
         int expected = 0;
-        int actual = sqrService.findSqr(minLimit,maxLimit);
-        assertEquals(expected,actual);
+        int actual = sqrService.findSqr(minLimit, maxLimit);
+        assertEquals(expected, actual);
     }
+
     @Test
     void shouldFindSqrInTask() {
         SQRService sqrService = new SQRService();
         int minLimit = 200;
         int maxLimit = 300;
         int expected = 3;
-        int actual = sqrService.findSqr(minLimit,maxLimit);
-        assertEquals(expected,actual);
+        int actual = sqrService.findSqr(minLimit, maxLimit);
+        assertEquals(expected, actual);
     }
 }
